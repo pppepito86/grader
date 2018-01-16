7d569f5408c8c24341fb28b7a43b8f35267a840b
Petar Petrov <pppepito86@gmail.com>
2018-01-17 00:10:16 +0200

    delete work dir

--- src/main/java/org/pesho/grader/SubmissionGrader.java
+++ src/main/java/org/pesho/grader/SubmissionGrader.java
@@ -41,7 +41,7 @@ public class SubmissionGrader {
 			if (score > 0) FileUtils.deleteQuietly(sandboxDir);
 			return score;
 		} finally {
-//			FileUtils.deleteQuietly(sandboxDir);
+			FileUtils.deleteQuietly(sandboxDir);
 		}
 	}
 	
