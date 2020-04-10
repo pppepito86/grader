1c667b49c84936d7d635675970be7dcf16854426
Petar Petrov <pppepito86@gmail.com>
2020-04-10 03:58:04 +0300

    Revert "increase compile step memory"
    This reverts commit 9b9aaae849219b5becccc799b68acf6c1d998767.

--- src/main/java/org/pesho/grader/compile/CompileStep.java
+++ src/main/java/org/pesho/grader/compile/CompileStep.java
@@ -34,7 +34,7 @@ public abstract class CompileStep implements BaseStep {
 				.map(command -> new SandboxExecutor()
 						.directory(sandboxDir)
 						.timeout(30)
-						.memory(2024)
+						.memory(1024)
 						.command(command)
 						.execute().getResult())
 				.map(x -> getResult(x))
