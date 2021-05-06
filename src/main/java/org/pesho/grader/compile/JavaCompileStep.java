86ca41d1dd0e0fc74c2ac5976a6c6a1c92f75858
Petar Petrov <pppepito86@gmail.com>
2021-05-06 07:52:18 +0300

    fix packages

--- src/main/java/org/pesho/grader/compile/JavaCompileStep.java
+++ src/main/java/org/pesho/grader/compile/JavaCompileStep.java
@@ -38,7 +38,7 @@ public class JavaCompileStep extends CompileStep {
 					.execute()
 					.outputString()
 					.trim();
-			return fullPath.replaceAll(SOURCE_FILE_ENDING + "$", "");
+			return fullPath.replaceAll(SOURCE_FILE_ENDING + "$", "").replaceAll("/", ".").replaceAll("..", ".");
 		} catch (Exception e) {
 			e.printStackTrace();
 			return sourceFile.getName().replaceAll(SOURCE_FILE_ENDING + "$", "");
