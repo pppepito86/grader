1121b4556d6ec9c3967f48162ac944795d7220f1
Petar Petrov <pppepito86@gmail.com>
2022-05-25 11:40:54 +0300

    add files

---
+++ src/main/java/org/pesho/grader/task/Criteria.java
@@ -0,0 +1,42 @@
+package org.pesho.grader.task;
+
+public class Criteria {
+	
+	private String reason;
+	private Double score;
+	private Double step;
+	private Boolean indent;
+	
+	public String getReason() {
+		return reason;
+	}
+	
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+	
+	public Double getScore() {
+		return score;
+	}
+	
+	public void setScore(double score) {
+		this.score = score;
+	}
+	
+	public Double getStep() {
+		return step;
+	}
+	
+	public void setStep(double step) {
+		this.step = step;
+	}
+	
+	public Boolean isIndent() {
+		return indent;
+	}
+	
+	public void setIndent(boolean indent) {
+		this.indent = indent;
+	}
+	
+}
---
+++ src/main/java/org/pesho/grader/task/parser/CriteriaFinder.java
@@ -0,0 +1,22 @@
+package org.pesho.grader.task.parser;
+
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class CriteriaFinder {
+
+	public static Optional<Path> find(List<Path> paths) {
+		List<String> candidates = Arrays.asList("criteria.json");
+		
+		paths = paths.stream()
+				.filter(x -> candidates.contains(x.getFileName().toString().toLowerCase()))
+				.collect(Collectors.toList());
+		if (paths.size() == 1) return paths.stream().findFirst();
+
+		return Optional.empty();
+	}
+	
+}
