923ff53b2af7071b5c6190ca138df3fb55926db0
Petar Petrov <pppepito86@gmail.com>
2023-02-18 15:12:53 +0200

    add ManagerFinder

---
+++ src/main/java/org/pesho/grader/task/parser/ManagerFinder.java
@@ -0,0 +1,18 @@
+package org.pesho.grader.task.parser;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class ManagerFinder {
+
+	public static Optional<Path> find(List<Path> paths) {
+		paths = paths.stream()
+				.filter(x -> x.getFileName().toString().equalsIgnoreCase("manager.cpp"))
+				.collect(Collectors.toList());
+		if (paths.size() == 1) return paths.stream().findFirst().map(p -> p.getParent());
+		return paths.stream().findFirst();
+	}
+	
+}
